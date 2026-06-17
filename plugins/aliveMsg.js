5b7af3edf9bb08a09949acc3c5d96890de93b748
thinura-nethsara <thinuranethsara2009@gmail.com>
2026-06-17 16:27:09 +0530

    Update aliveMsg.js

--- plugins/aliveMsg.js
+++ plugins/aliveMsg.js
@@ -2,21 +2,24 @@ function getAliveMessage() {
     const date = new Date().toLocaleDateString('en-US', { day: '2-digit', month: '2-digit', year: 'numeric' });
     const time = new Date().toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: false });
 
-    return `*{BOT_NAME} 𝐈𝐒 𝐎𝐍𝐋𝐈𝐍𝐄 💞*
+    return ` ◉◉◉◉◉━⋆─⋆──❂
+┊ ┊ ┊ ┊ ┊
+┊ ┊ ✫ ˚㋛ ⋆｡ ❀
+┊ ☠️︎︎
+✧ {BOT_NAME}✍️︎𝄞
+╰────────────────❂
 
-*╭────♡◉◉◉♡────⌬*
-💖 *Hey...I’m {BOT_NAME}🙃, your lovely assistant — alive and sparkling now!*
-*╰────♡◉◉◉♡────⌬*
+*╭◉◉◉────♡♡───❥❥❥*
+💖 *Hey...I’m {BOT_NAME}🤗, your lovely assistant — Now alive and Ready To Help!*
+*╰◉◉◉────♡♡───❥❥❥*
 
-*📅 ᴅᴀᴛᴇ: ${date}*
-*⌚ ᴛɪᴍᴇ: ${time}*
-*───────────────*
 
-*📱 ɴᴜᴍʙᴇʀ: {OWNER_NUMBER}*
-*💬 ᴘʀᴇꜰɪx: {PREFIX}*
-*───────────────*
-*🌐 Contact US*
-> http://wa.me/+94774571418?text=*Hey__Suduu*
+┏━「 INFORMATION 」
+┃ *📅 ᴅᴀᴛᴇ: ${date}*
+┃ *⌚ ᴛɪᴍᴇ: ${time}*
+┃ *💬 ᴘʀᴇꜰɪx: {PREFIX}*
+┗━━━━━━━━━━━━━◉◉◉
+
 
 > *_𝐏𝐎𝐖𝐄𝐑𝐄𝐃 𝐁𝐘 𝐙𝐄𝐔𝐒 𝐈𝐍𝐂 </>_ 🇱🇰*`;
 }
